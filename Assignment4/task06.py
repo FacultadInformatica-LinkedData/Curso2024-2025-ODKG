73541b40c826224b2f96c3f30b437e747d430242
RodrigoMartin-GomezdelMoral-Herranz <rodrigo.gomezdelmoral.herranz@alumnos.upm.es>
2024-10-04 13:07:50 +0200

    Assignment 4

---
+++ Assignment4/task06.py
@@ -0,0 +1,96 @@
+# %% [markdown]
+# **Task 06: Modifying RDF(s)**
+
+# %%
+!pip install rdflib
+github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2024-2025/master/Assignment4/course_materials"
+
+# %% [markdown]
+# Read the RDF file as shown in class
+
+# %%
+from rdflib import Graph, Namespace, Literal
+from rdflib.namespace import RDF, RDFS
+g = Graph()
+g.namespace_manager.bind('ns', Namespace("http://somewhere#"), override=False)
+g.namespace_manager.bind('vcard', Namespace("http://www.w3.org/2001/vcard-rdf/3.0#"), override=False)
+g.parse(github_storage+"/rdf/example5.rdf", format="xml")
+
+# %% [markdown]
+# Create a new class named Researcher
+
+# %%
+ns = Namespace("http://somewhere#")
+g.add((ns.Researcher, RDF.type, RDFS.Class))
+for s, p, o in g:
+  print(s,p,o)
+
+# %% [markdown]
+# **TASK 6.1: Create a new class named "University"**
+# 
+
+# %%
+# TO DO
+g.add((ns.University, RDF.type, RDFS.Class))
+# Visualize the results
+for s, p, o in g:
+  print(s,p,o)
+
+# %% [markdown]
+# **TASK 6.2: Add "Researcher" as a subclass of "Person"**
+
+# %%
+# TO DO
+g.add((ns.Researcher, RDFS.subClassOf, ns.Person))
+# Visualize the results
+for s, p, o in g:
+  print(s,p,o)
+
+# %% [markdown]
+# **TASK 6.3: Create a new individual of Researcher named "Jane Smithers"**
+
+# %%
+# TO DO
+g.add((ns.JaneSmithers, RDF.type, ns.Researcher))
+# Visualize the results
+for s, p, o in g:
+  print(s,p,o)
+
+# %% [markdown]
+# **TASK 6.4: Add to the individual JaneSmithers the email address, fullName, given and family names. Use the https://schema.org vocabulary**
+
+# %%
+# TO DO
+s1 = Namespace('https://schema.org/')
+g.add((ns.JaneSmith, s1.email, Literal("jane.smithers@example.org")))
+g.add((ns.JaneSmithers, s1.name, Literal("Jane Smithers")))
+g.add((ns.JaneSmithers, s1.givenName, Literal("Jane")))
+g.add((ns.JaneSmithers, s1.familyName, Literal("Smithers")))
+# Visualize the results
+for s, p, o in g:
+  print(s,p,o)
+
+# %% [markdown]
+# **TASK 6.5: Add UPM as the university where John Smith works. Use the "https://example.org/ namespace**
+
+# %%
+# TO DO
+s2 = Namespace("https://example.org/")
+g.add((s2.UPM, RDF.type, ns.University))
+g.add((ns.JohnSmith, s2.worksAt, s2.UPM))
+# Visualize the results
+for s, p, o in g:
+  print(s,p,o)
+
+# %% [markdown]
+# **Task 6.6: Add that Jown knows Jane using the FOAF vocabulary. Make sure the relationship exists.**
+
+# %%
+# TO DO
+foaf = Namespace("http://xmlns.com/foaf/spec")
+g.add((ns.JohnSmith, foaf.knows, ns.JaneSmithers))
+# Visualize the results
+for s, p, o in g:
+  print(s,p,o)
+
+
