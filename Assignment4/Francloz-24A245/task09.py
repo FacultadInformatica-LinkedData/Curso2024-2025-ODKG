992ed2d43c877e669d66392879658aa6dd7f090d
Francloz <franbloz98@gmail.com>
2024-10-07 23:00:44 +0200

    Assignment 4 file check and task 8

--- Assignment4/Francloz-24A245/task09.py
+++ Assignment4/Francloz-24A245/task09.py
@@ -9,14 +9,21 @@ Original file is located at
 **Task 09: Data linking**
 """
 
-!pip install rdflib
-github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2024-2025/master/Assignment4/"
+github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2024-2025/master/Assignment4/course_materials"
 
 from rdflib import Graph, Namespace, Literal, URIRef
+
 g1 = Graph()
 g2 = Graph()
 g3 = Graph()
-g1.parse(github_storage+"resources/data03.rdf", format="xml")
-g2.parse(github_storage+"resources/data04.rdf", format="xml")
+g1.parse(github_storage+"/rdf/data03.rdf", format="xml")
+g2.parse(github_storage+"/rdf/data04.rdf", format="xml")
+
+
+vcard = Namespace("http://www.w3.org/2001/vcard-rdf/3.0#")
+schema = Namespace("http://schema.org/")
+ns = Namespace("http://somewhere#")
+g.namespace_manager.bind('ns', Namespace("http://somewhere#"), override=False)
+g.namespace_manager.bind('vcard', Namespace("http://www.w3.org/2001/vcard-rdf/3.0#"), override=False)
 
 """Busca individuos en los dos grafos y enlázalos mediante la propiedad OWL:sameAs, inserta estas coincidencias en g3. Consideramos dos individuos iguales si tienen el mismo apodo y nombre de familia. Ten en cuenta que las URI no tienen por qué ser iguales para un mismo individuo en los dos grafos."""
