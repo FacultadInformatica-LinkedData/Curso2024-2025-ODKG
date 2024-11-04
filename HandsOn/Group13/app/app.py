a77976a478226ae0bd4d970b2fb5873335d9f867
Luis <102657312+Lous13@users.noreply.github.com>
2024-11-04 18:21:55 +0100

    Add files via upload

---
+++ HandsOn/Group13/app/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, jsonify, g, request, render_template
+import rdflib
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Habilitar CORS para toda la aplicación
+
+# Cargar el grafo RDF en memoria
+@app.before_request
+def load_graph():
+    g.graph = rdflib.Graph()
+    g.graph.parse("CCTV-via-publica-updated-with-links.ttl", format="ttl")
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # Renderiza index.html
+
+@app.route('/api/locations', methods=['GET'])
+def get_locations():
+    locations = set()
+    for camera in g.graph.subjects(rdflib.RDF.type, rdflib.URIRef("http://CamaraMadrid.org/Camera")):
+        location = g.graph.value(camera, rdflib.URIRef("http://CamaraMadrid.org/location"))
+        if location:
+            locations.add(location)
+
+    return jsonify(list(locations))
+
+@app.route('/api/cameras', methods=['GET'])
+def get_cameras():
+    location = request.args.get('location')
+    cameras = []
+    type_count = {}
+    resolution_count = {}
+    zoom_count = {}
+
+    for camera in g.graph.subjects(rdflib.RDF.type, rdflib.URIRef("http://CamaraMadrid.org/Camera")):
+        cam_location = g.graph.value(camera, rdflib.URIRef("http://CamaraMadrid.org/location"))
+        if cam_location and str(cam_location) == location:
+            camera_data = {
+                'id': camera.split('/')[-1],
+                'type': str(g.graph.value(camera, rdflib.URIRef("http://CamaraMadrid.org/type"))),
+                'resolution': str(g.graph.value(camera, rdflib.URIRef("http://CamaraMadrid.org/resolution"))),
+                'zoom': str(g.graph.value(camera, rdflib.URIRef("http://CamaraMadrid.org/opticalZoom"))),
+                'year': str(g.graph.value(camera, rdflib.URIRef("http://CamaraMadrid.org/acquisitionYear"))),
+            }
+            cameras.append(camera_data)
+
+            if camera_data['type'] in type_count:
+                type_count[camera_data['type']] += 1
+            else:
+                type_count[camera_data['type']] = 1
+
+            if camera_data['resolution'] in resolution_count:
+                resolution_count[camera_data['resolution']] += 1
+            else:
+                resolution_count[camera_data['resolution']] = 1
+
+            if camera_data['zoom'] in zoom_count:
+                zoom_count[camera_data['zoom']] += 1
+            else:
+                zoom_count[camera_data['zoom']] = 1
+
+    return jsonify({
+        'cameras': cameras,
+        'type_count': type_count,
+        'resolution_count': resolution_count,
+        'zoom_count': zoom_count
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
